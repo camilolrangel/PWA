b57c0687f33a61748ddd3f3cc2440d3d8bb48d43
camilolrangel <61031278+camilolrangel@users.noreply.github.com>
2020-02-20 19:18:43 -0300

    Primeiro Projeto
    Primeiro projeto

---
+++ Blog/Blog/Models/Blog/Postagem/PostagemEntity.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Models.Blog.Autor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.Blog.Postagem
+{
+    public class PostagemEntity
+    {
+        public string Titulo;
+        public AutorEntity autor;
+
+    }
+
+    public class PostagemSobreFilmesEntity : PostagemEntity
+    {
+        public string Genero;
+    }
+}
