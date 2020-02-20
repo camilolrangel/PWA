0fd6b72dd4be92aa9c1804fc09de4557cbad786f
camilolrangel <61031278+camilolrangel@users.noreply.github.com>
2020-02-20 20:46:59 -0300

    Segunda aula - Criacao de novas classes models

--- Blog/Blog/Models/Blog/Postagem/PostagemEntity.cs
+++ Blog/Blog/Models/Blog/Postagem/PostagemEntity.cs
@@ -1,4 +1,7 @@
 ﻿using Blog.Models.Blog.Autor;
+using Blog.Models.Blog.Categoria;
+using Blog.Models.Blog.Etiqueta;
+using Blog.Models.Blog.Postagem.Revisao;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +12,11 @@ namespace Blog.Models.Blog.Postagem
     public class PostagemEntity
     {
         public string Titulo;
-        public AutorEntity autor;
-
+        public AutorEntity Autor;
+        public CategoriaEntity Categoria;
+        public List<EtiquetaEntity> Etiquetas;
+        public List<RevisaoEntity> Revisoes;
     }
 
-    public class PostagemSobreFilmesEntity : PostagemEntity
-    {
-        public string Genero;
-    }
+    
 }
