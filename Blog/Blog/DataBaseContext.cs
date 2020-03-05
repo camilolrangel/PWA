5e66d854c0a555aaf86be76b97ec05dba3d2f9a2
camilolrangel <61031278+camilolrangel@users.noreply.github.com>
2020-03-05 20:38:17 -0300

    Conexão com o Mysql
    Conexão com Mysql

---
+++ Blog/Blog/DataBaseContext.cs
@@ -0,0 +1,22 @@
+﻿
+using Blog.Models.Blog.Categoria;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog
+{
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<CategoriaEntity> categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseMySql("Server=localhost;User=root;password=root;Database=pwablog ");
+        }
+    }
+}
--- Blog/Blog/Models/Blog/Categoria/CategoriaEntity.cs
+++ Blog/Blog/Models/Blog/Categoria/CategoriaEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,10 @@ namespace Blog.Models.Blog.Categoria
 {
     public class CategoriaEntity
     {
-        public string Nome;
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Nome { get; set; }
     }
 }
